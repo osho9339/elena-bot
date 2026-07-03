0f735e9aa32c76a3f06aa73f7b9e46d55b3e7f1f
osho9339 <osho9339@gmail.com>
2026-07-03 22:42:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ import telebot
 from flask import Flask, request
 
 app = Flask(__name__)
-token = os.environ.get("TELEGRAM_BOT_TOKEN")
-print(f"DEBUG: Token loaded is: {token}") # This will print to your logs
-bot = telebot.TeleBot(token)
+bot = telebot.TeleBot(os.environ.get("TELEGRAM_BOT_TOKEN"))
 
 @app.route('/', methods=['POST'])
 def webhook():
@@ -14,12 +12,6 @@ def webhook():
     bot.process_new_updates([update])
     return 'OK', 200
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, "I received your message!")
-    return 'Forbidden', 403
-
-# This handler listens for EVERY message
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, "I received your message!")
